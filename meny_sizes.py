9c2c91bb4316bb717319b01dae8e6b79b52c2800
Avraham <avibaron78@gmail.com>
2025-03-30 12:16:25 +0300

    add script to generate parquet files of varying sizes from historical data

---
+++ meny_sizes.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+import os
+
+file = 'history/parquets/1INCH_USDT.parquet'
+df = pd.read_parquet(file)
+df = df[['time', 'open', 'high', 'low', 'close']]
+
+os.makedirs('sizes', exist_ok=True)
+
+sizes = [5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000]
+
+for size in sizes:
+    df.iloc[:size].to_parquet(f'sizes/{size}.parquet', index=False)
