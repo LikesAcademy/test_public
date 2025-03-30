d7139b74cb198f4886ce86ea38c73865c650832f
Avraham <avibaron78@gmail.com>
2025-03-30 14:15:44 +0300

    update data export to include last 30,000 rows and ensure CSV conversion

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ import shutil
 parquet_folder = 'history/parquets'
 csv_folder = 'history/csv'
 
-# if os.path.exists(csv_folder):
-#     shutil.rmtree(csv_folder)
-# os.makedirs(csv_folder)
+if os.path.exists(csv_folder):
+    shutil.rmtree(csv_folder)
+os.makedirs(csv_folder)
 
-# for file in os.listdir(parquet_folder):
-#     if file.endswith('.parquet'):
-#         df = pd.read_parquet(os.path.join(parquet_folder, file))
-#         df = df[['time', 'open', 'close', 'high', 'low']]
-#         csv_path = os.path.join(csv_folder, file.replace('.parquet', '.csv'))
-#         df.to_csv(csv_path, index=False)
-#         print(f'Converted {file} to {csv_path}')
+for file in os.listdir(parquet_folder):
+    if file.endswith('.parquet'):
+        df = pd.read_parquet(os.path.join(parquet_folder, file))
+        df = df[['time', 'open', 'close', 'high', 'low']]
+        df = df.tail(30000)
+        csv_path = os.path.join(csv_folder, file.replace('.parquet', '.csv'))
+        df.to_csv(csv_path, index=False)
+        print(f'Converted {file} to {csv_path}')
 
 def ticker(symbols_list):
     url = 'https://contract.mexc.com/api/v1/contract/ticker'
