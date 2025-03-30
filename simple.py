3cc75e0189a4fe3d5e807a7af4d9566c3b3b391f
Avraham <avibaron78@gmail.com>
2025-03-30 12:05:26 +0300

    update data export to save as both CSV and Parquet formats

--- simple.py
+++ simple.py
@@ -7,5 +7,6 @@ url = f'https://contract.mexc.com/api/v1/contract/kline/{symbol}'
 response = requests.get(url)
 data = response.json()
 df = pd.DataFrame(data['data'])
-df.to_csv('symbol.csv', index=False)
+df.to_csv(f'{symbol}.csv', index=False)
+df.to_parquet(f'{symbol}.parquet', index=False)
 print(df.head())
